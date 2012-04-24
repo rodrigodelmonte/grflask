4f2adaddd5184c004faa19028598885ea3768d07
Rodrigo <rodrigo.dexter@gmail.com>
2012-04-24 19:07:54 -0300

    Add current layout from granderock.com.

--- app.py
+++ app.py
@@ -6,8 +6,11 @@ from flask import url_for
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def index():
     return render_template('index.html')
+
+with app.test_request_context():
+    url_for('static', filename='style.css')
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
