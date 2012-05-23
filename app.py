7f0ca0209ded136409bf2ebd463e1737852541e0
Rodrigo <rodrigo.dexter@gmail.com>
2012-05-23 17:24:27 -0300

    Adding favicon.ico

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from flask import render_template
 from flask import make_response
+from flask import send_from_directory
 
 app = Flask(__name__)
 
@@ -19,6 +20,10 @@ def robots():
     response.headers['Content-Type'] = 'text/plain'
     return response
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static/img/'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
