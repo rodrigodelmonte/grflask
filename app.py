59feb2f2b6c23b15c34afa43a3ce3475baa142ac
Rodrigo Vieira <rodrigo.dexter@gmail.com>
2012-09-25 22:48:20 -0300

    Testeting pagsocial.

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/teste')
+def teste():
+    return render_template('index2.html')
+
 @app.route('/robots.txt')
 def robots():
     response = make_response(render_template('robots.txt'))
@@ -19,7 +23,7 @@ def robots():
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static/img/'),
-                              'favicon.ico', mimetype='image/vnd.microsoft.icon')
+                            'favicon.ico', mimetype='image/vnd.microsoft.icon')
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
